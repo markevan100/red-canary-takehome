b914eb50d72881d24d88390b1a5a08b74f9ca0bf
Mark Modak <markevan100@gmail.com>
2024-11-04 10:56:06 -0500

    Add call to handle network action

---
+++ src/actions/network_action.py
@@ -0,0 +1,2 @@
+def handle_network_action():
+    print("handle_network_action has been called but is currently blank")
--- src/telemetry_tester.py
+++ src/telemetry_tester.py
@@ -1,11 +1,12 @@
 from actions.process_action import handle_process_action
 from actions.file_action import handle_file_action
+from actions.network_action import handle_network_action
 
 def main():
     print("Welcome to Modak's EDR Telemetry Test Program")
     
     while True:
-        print("\nAvailable actions: process_start (ps), file_action (fa), exit")
+        print("\nAvailable actions: process_start (ps), file_action (fa), network_connect (nc), exit")
         action = input("What action would you like to test? ").strip().lower()
 
         if action in ["ps", "process_start"]:
@@ -15,6 +16,10 @@ def main():
         elif action in ["fa", "file_action"]:
             print("File action selected.")
             handle_file_action()
+
+        elif action in ["nc", "network_connect"]:
+            print("Network connect selected.")
+            handle_network_action()
 
         elif action == "exit":
             print("Exiting the Telemetry Test Program.")
